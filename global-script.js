ba067aefb70be58f1ca954b281b6a5d5dbea5560
Akshay Agrawal <41827133+axayagrawal@users.noreply.github.com>
2024-01-08 22:24:07 +0530

    Create global-script.js

---
+++ global-script.js
@@ -0,0 +1,16 @@
+<script>
+
+
+$('.button').on('mouseenter', function() {
+  $(this).addClass('on-hover');
+  $(this).find('.button-icon-wrapper').addClass('on-hover');
+  $(this).find('.button-icon').addClass('on-hover');
+});
+
+$('.button').on('mouseleave', function() {
+  $(this).removeClass('on-hover');
+  $(this).find('.button-icon-wrapper').removeClass('on-hover');
+  $(this).find('.button-icon').removeClass('on-hover');
+});
+
+</script>
