857db3f58575c941d0747b56d7e43d6956cec565
Akshay Agrawal <41827133+axayagrawal@users.noreply.github.com>
2024-01-08 22:26:48 +0530

    Update global-script.js

--- global-script.js
+++ global-script.js
@@ -1,6 +1,3 @@
-<script>
-
-
 $('.button').on('mouseenter', function() {
   $(this).addClass('on-hover');
   $(this).find('.button-icon-wrapper').addClass('on-hover');
@@ -12,5 +9,3 @@ $('.button').on('mouseleave', function() {
   $(this).find('.button-icon-wrapper').removeClass('on-hover');
   $(this).find('.button-icon').removeClass('on-hover');
 });
-
-</script>
