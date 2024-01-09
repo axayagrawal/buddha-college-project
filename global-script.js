ea4ae67c771161188bd54413d2650dbbe5403fb6
Akshay Agrawal <41827133+axayagrawal@users.noreply.github.com>
2024-01-09 17:25:05 +0530

    Button Outline Update

--- global-script.js
+++ global-script.js
@@ -9,3 +9,17 @@ $('.button').on('mouseleave', function() {
   $(this).find('.button-icon-wrapper').removeClass('on-hover');
   $(this).find('.button-icon').removeClass('on-hover');
 });
+
+// Button Outlne
+
+$('.button-outline').on('mouseenter', function() {
+  $(this).addClass('on-hover');
+  $(this).find('.button-outline-icon-wrapper').addClass('on-hover');
+  $(this).find('.button-icon').addClass('on-hover');
+});
+
+$('.button-outline').on('mouseleave', function() {
+  $(this).removeClass('on-hover');
+  $(this).find('.button-outline-icon-wrapper').removeClass('on-hover');
+  $(this).find('.button-icon').removeClass('on-hover');
+});
